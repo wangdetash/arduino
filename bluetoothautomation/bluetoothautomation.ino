82d1debc0b2f17948f9fbb0dfcee4ecc73d17251
wangdetash <wangdetash@gmail.com>
2017-07-16 12:30:11 +0530

    adding lcd module

--- bluetoothautomation/bluetoothautomation.ino
+++ bluetoothautomation/bluetoothautomation.ino
@@ -1,26 +1,51 @@
+The circuit:
+ * LCD RS pin to digital pin 12
+ * LCD Enable pin to digital pin 11
+ * LCD D4 pin to digital pin 5
+ * LCD D5 pin to digital pin 4
+ * LCD D6 pin to digital pin 3
+ * LCD D7 pin to digital pin 2
+ * LCD R/W pin to ground
+ * 10K resistor:
+ * ends to +5V and ground
+ * wiper to LCD VO pin (pin 3)
+ 
+ 
 #include <SoftwareSerial.h>
-int value;
-LiquidCrystal LCD(12, 11, 7, 6, 5, 4);
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+int a;
 void setup()
 {
-  Serial.begin(9600);
+  Serial.begin(9600);    
+  Serial.write("HELLO TASHI\n");  
   pinMode(13,OUTPUT);
-  LCD.setCursor(0,0);
-  LCD.print("SWITCH STATUS");
- 
+    lcd.begin(16, 2);
+     lcd.print("hello, TASHI");
+
+  
+  
 }
 void loop()
 {
-  if(Serial.available()==1)
+  if(Serial.available())
   {
-    value=Serial.read();
-  if(value==1)
+    a=Serial.read();
+  if(a=='1')
   {
     digitalWrite(13,HIGH);
+    Serial.write("The switch is ON\n");
+    lcd.clear();
+    lcd.setCursor(16, 1);
+     lcd.print("The switch is ON");
   }
-  else
+  else if(a=='0')
   {
     digitalWrite(13,LOW);
+    Serial.write("The switch is OFF\n");
+    lcd.clear();
+    lcd.setCursor(16, 1);
+     lcd.print("The switch is OFF");
   }
   }
 }
