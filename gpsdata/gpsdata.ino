edafa102e6aa629c914dbe114a231e19086413f5
wangdetash <wangdetash@gmail.com>
2017-09-02 00:02:15 +0530

    get gps data using string functions

--- gpsdata/gpsdata.ino
+++ gpsdata/gpsdata.ino
@@ -1,19 +1,19 @@
 //program to find gps location using arduno and display it on lcd display
 
 #include <SoftwareSerial.h>
-#include<string.h>
 SoftwareSerial mySerial(8,9);
-char gps[100];
+
 
 void setup()
 {
-  
   Serial.begin(9600);
   mySerial.begin(9600);
-  Serial.print("GPS INITIALISING\n");
- delay(1000);
- Serial.print("................\n");
- delay(1000);
+  Serial.println("GPS INITIALISING");
+}
+
+
+void loop()
+{
 while(mySerial.available()==0);
 String gps = mySerial.readStringUntil('\r');
 if(gps.substring(0,6)=="$GPGGA")
@@ -28,13 +28,8 @@ Serial.print("LAT:");
 Serial.println(gps.substring(14,29));
 Serial.print("LONG:");
 Serial.println(gps.substring(30,42));
+delay(1000);
 }
-}
-
-
-void loop()
-{
-
 }
 
 
